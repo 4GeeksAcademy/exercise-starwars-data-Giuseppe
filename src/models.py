3ad99258c391112dc0d55767346eed345f00158e
Giuseppe <132507608+GiuseppeTaylorr@users.noreply.github.com>
2023-08-18 22:21:12 +0000

    finish

--- src/models.py
+++ src/models.py
@@ -25,6 +25,38 @@ class Address(Base):
     person_id = Column(Integer, ForeignKey('person.id'))
     person = relationship(Person)
 
+class Favorite(Base):
+    __tablename__ = 'favorite'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    planet_id = Column(Integer, ForeignKey('planet.id'))
+    personaje_id = Column(Integer, ForeignKey('personaje.id'))
+    person_id = Column(Integer, ForeignKey('person.id'))
+    
+
+class Planets(Base):
+    __tablename__ = 'planets'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    clima = Column(String(250))
+    poblacion = Column(String(250), nullable=False)
+    favorite_id = Column(Integer, ForeignKey('favorite.id'))
+
+
+class Personaje(Base):
+    __tablename__ = 'personaje'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    id = Column(Integer, primary_key=True)
+    name = Column(String(250))
+    genero = Column(String(250))
+    fecha_de_nacimiento = Column(String(250), nullable=False)
+    favorite_id = Column(Integer, ForeignKey('favorite.id'))
+    
+
     def to_dict(self):
         return {}
 
